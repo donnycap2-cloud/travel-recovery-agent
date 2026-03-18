ec80d84c3a5823712260736297ffb6b68be2ac41
Donovan Capdeville <donnycap2@gmail.com>
2026-03-18 13:53:28 -0400

    fix: debug

--- components/ConnectionCountdown.tsx
+++ components/ConnectionCountdown.tsx
@@ -38,11 +38,8 @@ export default function ConnectionCountdown({
         const arrivalDate = new Date(arrival)
       
         // fix timezone (same as before)
-        const localDeparture =
-          departureDate.getTime() + departureDate.getTimezoneOffset() * 60000
-      
-        const localArrival =
-          arrivalDate.getTime() + arrivalDate.getTimezoneOffset() * 60000
+        const localDeparture = departureDate.getTime()
+        const localArrival = arrivalDate.getTime()
       
         const timeToDeparture = localDeparture - now
         const connectionMargin = localDeparture - localArrival
